65b308cd979ea4f66efadd1e277efa6468fce8fe
MarkoSopta <77636140+MarkoSopta@users.noreply.github.com>
2024-08-31 12:03:29 +0200

    webconfig

---
+++ src/main/java/com/hackathon/app/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.hackathon.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080/","http://localhost:8081/") // Specify the allowed origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow cookies and credentials
+    }
+}
